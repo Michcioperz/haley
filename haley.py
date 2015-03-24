82aad4a6a8bea9415f266428f9228fe617459aa8
Michał Sidor <michcioperz@gmail.com>
2015-03-24 23:21:28 +0100

    whole lotta love *guitar riff here*

---
+++ haley.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python2
+
+import socket, re, argparse, logging, threading
+
+LOGLEVEL_RECV = 18
+LOGLEVEL_SENT = 17
+
+logging.addLevelName(LOGLEVEL_RECV, "RECV")
+logging.addLevelName(LOGLEVEL_SENT, "SENT")
+logging.basicConfig(level=15, format='%(asctime)s %(levelname)s %(message)s')
+
+class Haley(threading.Thread):
+    def __init__(self, host, port, channel, nickname):
+        threading.Thread.__init__(self)
+        self.host = host
+        self.port = port
+        if "#" not in channel:
+            self.channel = "#%s" % channel
+        else:
+            self.channel = channel
+        self.nickname = nickname
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.filters = []
+    def register_filter(self, priority=1):
+        def func_wrapper(func):
+            self.filters.append((priority,func))
+            self.filters.sort()
+            return func
+        return func_wrapper
+    def send(self, message):
+        for line in message.split("\n"):
+            logging.log(LOGLEVEL_SENT, "< %s" % line)
+            self.socket.sendall("%s\r\n" % line)
+    def say(self, channel, message):
+        for line in message.split("\n"):
+            self.send("PRIVMSG %s :%s" % (channel, line))
+    def refresh(self):
+        execfile("filters.py", {"haley": self})
+    def run(self):
+        self.refresh()
+        self.socket.connect((self.host, self.port))
+        buff = ""
+        while True:
+            rc = self.socket.recv(512).replace("\r", "")
+            buff += rc
+            while "\n" in buff:
+                message = buff.split("\n", 1)[0]
+                buff = buff.split("\n",1)[1]
+                logging.log(LOGLEVEL_RECV, message)
+                if "checking ident" in message.lower():
+                    self.send("USER %s %s s: %s" % (self.nickname, self.nickname, self.nickname))
+                    self.send("NICK %s" % self.nickname)
+                elif "nickname is already in use" in message.lower():
+                    self.nickname += "_"
+                    self.send("NICK %s" % self.nickname)
+                elif message.upper().startswith("PING"):
+                    self.send("PONG%s" % message[4:])
+                elif message.startswith(":") and ("001 %s :" % self.nickname) in message and "PRIVMSG" not in message.upper():
+                    self.send("JOIN %s" % self.channel)
+                elif ("PRIVMSG %s " % self.channel) in message.split(":")[1]:
+                    friend = message.split(":")[1].split(" ")[0].split("!")[0]
+                    if friend != self.nickname:
+                        for fill in self.filters:
+                            if fill[1](self, message.split(":", 2)[2], friend):
+                                break
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Simple IRC bot")
+    parser.add_argument("hostname", help="IRC server's IP or hostname")
+    parser.add_argument("channel", help="Channel to mess with, leading '#' not required")
+    parser.add_argument("-p", "--port", default=6667, metavar="port", help="IRC server's port")
+    parser.add_argument("-n", "--name", default="hal3y", metavar="name", help="Nickname for the bot to use")
+    args = parser.parse_args()
+    overlord = Haley(args.hostname, args.port, args.channel, args.name)
+    overlord.start()
